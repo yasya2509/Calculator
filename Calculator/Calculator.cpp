d05cd4d2454016c085d1cf576dc211fd3616efb5
M.Silin <yaroslavasiiliina@gmail.com>
2022-11-23 18:09:45 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double Calculate(double a, char operation, double b)
+{
+    switch (operation) {
+    case '+':
+        cout << "a + b = " << a + b << endl;
+        break;
+    case '-':
+        cout << "a - b = " << a - b << endl;
+        break;
+    case '*':
+        cout << "a * b = " << a * b << endl;
+        break;
+    case '/':
+        cout << "a / b = " << a / b << endl;
+        break;
+    }
+    return 0;
+}
+
+int main()
+{
+    double a, b;
+    char operation;
+    cin >> a >> operation >> b;
+    cout << Calculate(a, operation, b) << endl;
+
+    return 0;
+  
+} 
+
